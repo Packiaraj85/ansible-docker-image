915695242ae24684f743039cb867b25d5bd86bc1
Packiaraj Daniel <80152768+Packiaraj85@users.noreply.github.com>
2024-02-17 11:10:13 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-ssh-keygen -q -t rsa -N '' -f ./id_rsa <<<y 2>&1 >/dev/null
+ssh-keygen -q -t rsa -N '' -f ./id_rsa
 docker build -t packiarajd/docker-ansible:ver.1 .
 docker run --rm -it docker-ansible ansible --version
