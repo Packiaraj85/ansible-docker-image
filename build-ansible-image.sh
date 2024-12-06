77bf99d8cc6673108991490a23e2277ebc2433e4
Packiaraj D <packiarajd@idp.local@hdp.idp.local>
2024-12-06 08:28:36 +0530

    update

--- build-ansible-image.sh
+++ build-ansible-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ssh-keygen -q -t rsa -N '' -f ./id_rsa <<<y 2>&1 >/dev/null
-docker build -t packiarajd/docker-ansible:ver.1 .
-docker run --rm -it docker-ansible ansible --version
+docker build -t packiarajd/docker-ansible:ver.2 .
+docker run --rm -itd --name ansible_server packiarajd/docker-ansible:ver.2
